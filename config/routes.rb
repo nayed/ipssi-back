cd591ff5997721c09bc6cc2ded36e3ed5b1b919d
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-01-12 16:32:54 +0100

    I'm stupid

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post 'user_token' => 'user_token#create'
       
-      resources :articles, only: [:index, :show]
-      resources :articles, except: [:index, :show]
+      resources :articles
     end
   end
 end
